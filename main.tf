9a6548c0152e1c3eeef3ff9b797739772efaf502
Bansil Patel <47664796+Bansil06@users.noreply.github.com>
2026-01-01 16:34:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,32 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["43.242.116.115/32"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["185.199.108.0/22"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["192.30.252.0/22"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["140.82.112.0/20"]
+  }
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["143.55.64.0/20"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
