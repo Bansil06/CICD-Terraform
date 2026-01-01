0d712499a9506ea21bd8879991e59a1dee7f82a9
Bansil Patel <47664796+Bansil06@users.noreply.github.com>
2026-01-01 21:03:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,14 @@
-resource "aws_security_group" "web_sg" {
-  name = "web-sg"
-  tags = {
-    Name = "Jenkins-Terraform"
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["43.242.116.115/32"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["43.242.116.115/32"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["185.199.108.0/22"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["192.30.252.0/22"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["140.82.112.0/20"]
-  }
-  
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["143.55.64.0/20"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "existing_sg" {
+  id = "sg-0b724ef2b7afbbc1a"
 }
 
 resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
   tags = {
-    Name = "jenkins-terraform-ec2"
+    Name = "Jenkins-Terraform"
   }
 }
